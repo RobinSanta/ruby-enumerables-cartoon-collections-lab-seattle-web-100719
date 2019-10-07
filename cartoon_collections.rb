2ff343af5b1785875c635ff8a9516fe1ec5b2a05
Robinson Santana Silverio <robinsntn@gmail.com>
2019-10-07 10:11:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,17 +1,17 @@
 def roll_call_dwarves(array)
   i = 0
   while i < array.length
-  puts "#{i + 1}. #{array[i]}"
-  i += 1 
-end
-end
-
-def summon_captain_planet# code an argument here
-  # Your code here
+    puts "#{i + 1}. #{array[i]}"
+    i += 1 
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def summon_captain_planet
+  
+end
+
+def long_planeteer_calls
+  
 end
 
 def find_the_cheese# code an argument here
