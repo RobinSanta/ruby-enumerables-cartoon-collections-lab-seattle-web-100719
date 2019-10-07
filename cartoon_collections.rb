93e31f98a81b143779345ee5be5cc0f14da8427c
Robinson Santana Silverio <robinsntn@gmail.com>
2019-10-07 10:31:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,7 +26,16 @@ def long_planeteer_calls(array)
   end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
+  i = 0
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+
+
+
+
+
+
+
+
